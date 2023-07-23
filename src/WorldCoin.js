2de882885199d931c7603c4caffc4c6ba978caa2
Kenil shah <kenilshah1505@gmail.com>
2023-07-23 12:49:29 +0530

    Update WorldCoin.js

--- src/WorldCoin.js
+++ src/WorldCoin.js
@@ -1,11 +1,13 @@
 /** @format */
 
-import { IDKitWidget } from '@worldcoin/idkit';
+import { IDKitWidget ,solidityEncode} from '@worldcoin/idkit';
 
-function WorldCoin() {
+function WorldCoin({props}) {
   <IDKitWidget
     app_id="app_staging_037e4ab05939862897bb5e4590281a1c" // obtain this from developer.worldcoin.org
-    action="my_action"
+    action={solidityEncode(["uint256"] , ["verifyUser"])}
+  credentential_types = {[CreedentialType.Orb]}
+  address = {props.address}
     enableTelemetry
     onSuccess={(result) => console.log(result)} // pass the proof to the API or your smart contract
   />;
